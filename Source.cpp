790f99eea970e762f6b47c669ed3965687f897ab
Dongfeng Gu <gudongfeng@outlook.com>
2016-02-12 10:46:32 -0500

    add the max product algorithm , still need to add the graph diagram

--- Source.cpp
+++ Source.cpp
@@ -6,17 +6,20 @@
 
 // m is equal to 1 or 0, represent the probability x value when x is equal to 0 or 1
 // this function will return the message passing by specific function
-void applyFunctionValue(std::vector<std::vector<std::vector<double>>> &prob,
+void applySumProduct(std::vector<std::vector<std::vector<double>>> &prob,
+	int otherValues[3], int i, int j);
+void applyMaxProduct(std::vector<std::vector<std::vector<double>>> &prob,
 	int otherValues[3], int i, int j);
 
 void main()
 {
 	int const		_num = 10000, _cycle = 500; //cyc is the cycle time of sum-product alg.
-	int					_errorCount = 0;
+	int					_sum_errorCount = 0, _max_errorCount = 0;
 	int					_start = 1, _s, _again;
 	double			_BER = 0.0, _M = 0.0, _S;	// Gaussian noise parameters
 	double			_Y[_num][7], _Z[_num][7];		// Z is observed code
 	std::vector<std::vector<std::vector<double>>>			_prob;					// the probability for each transmitted bit code
+	std::vector<std::vector<std::vector<double>>>			_prob_max;
 
 	std::cout << "***************************************************************" << std::endl;
 	std::cout << "***  The origianl codeword (0,0,0,0,0,0,0) is a " << _num << "*7 array ***" << std::endl;
@@ -48,7 +51,9 @@ void main()
 			exit(1);
 		}
 
-		// generate the observed variable Z
+		/*
+		*	encoding process, add the gaussian random vaiable
+		*/
 		for (int i = 0; i < _num; i++)
 		{
 			for (int j = 0; j < 7; j++)
@@ -65,26 +70,29 @@ void main()
 		*/
 		for (int i = 0; i < _num; i++)
 		{
-			std::vector<std::vector<double>> temp;
+			std::vector<std::vector<double>> Num1;
+			std::vector<std::vector<double>> Num2;
 			for (int j = 0; j < 7; j++)
 			{
 				std::vector<double> temp1;
+				std::vector<double> temp2;
 				for (int m = 0; m < 2; m++)
 				{
-					// generate the initial probability value of x=1 or x=0 by observing z. 
+					// generate the initial probability value of x=1 or x=0 by observing z.
 					// when m=0,x=0   or m=1,x=1
 					temp1.push_back(log(Util::instance()->Xprobability(_S, m, _Z[i][j])));
+					temp2.push_back(Util::instance()->Xprobability(_S, m, _Z[i][j]));
 				}
-				// normalization
-				/*
-				double sum = temp1[0] + temp1[1];
-				temp1[0] = temp1[0] / sum;
-				temp1[1] = temp1[1] / sum;
-				*/
+				// normalize the value
+				temp2[0] = temp2[0] / (temp2[0] + temp2[1]);
+				temp2[1] = temp2[1] / (temp2[0] + temp2[1]);
+
 				// sotre the probability data
-				temp.push_back(temp1);
+				Num1.push_back(temp1);
+				Num2.push_back(temp2);
 			}
-			_prob.push_back(temp);
+			_prob.push_back(Num1);
+			_prob_max.push_back(Num2);
 		}
 
 		// decoding procedure
@@ -92,93 +100,137 @@ void main()
 		{
 			for (int i = 0; i < _num; i++)
 			{
-				// get the probability for different x 
+				// get the probability for different x
 				for (int j = 0; j < 7; j++)
 				{
 					if (j == 0){
 						// update the x1 probability , f1
 						int otherValues[3] = { 3, 5, 7 };
-						applyFunctionValue(_prob, otherValues, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 					}
 					else if (j == 1)
 					{
 						// update the x2 probability, f2
 						int otherValues[3] = { 3, 6, 7 };
-						applyFunctionValue(_prob, otherValues, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 					}
 					else if (j == 2)
 					{
 						// update the x3 probability, f1
 						int otherValues[3] = { 1, 5, 7 };
-						applyFunctionValue(_prob, otherValues, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 						// update the x3 probability, f2
 						int otherValues2[3] = { 2, 6, 7 };
-						applyFunctionValue(_prob, otherValues2, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues2, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 					}
 					else if (j == 3)
 					{
 						// update the x4 probability, f3
 						int otherValues[3] = { 5, 6, 7 };
-						applyFunctionValue(_prob, otherValues, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 					}
 					else if (j == 4)
 					{
 						// update the x5 probability, f3
 						int otherValues[3] = { 4, 6, 7 };
-						applyFunctionValue(_prob, otherValues, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 						// update the x5 probability, f1
 						int otherValues2[3] = { 1, 3, 7 };
-						applyFunctionValue(_prob, otherValues2, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues2, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 					}
 					else if (j == 5)
 					{
 						// update the x6 probability, f2
 						int otherValues[3] = { 2, 3, 7 };
-						applyFunctionValue(_prob, otherValues, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 						// update the x6 probability, f3
 						int otherValues2[3] = { 4, 5, 7 };
-						applyFunctionValue(_prob, otherValues2, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues2, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 					}
 					else if (j == 6)
 					{
 						// update the x7 probability, f1
 						int otherValues[3] = { 1, 3, 5 };
-						applyFunctionValue(_prob, otherValues, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 						// update the x7 probability, f2
 						int otherValues2[3] = { 2, 3, 6 };
-						applyFunctionValue(_prob, otherValues2, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues2, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 						// update the x7 probability, f3
 						int otherValues3[3] = { 4, 5, 6 };
-						applyFunctionValue(_prob, otherValues3, i, j);
+						// sum product algorithm
+						applySumProduct(_prob, otherValues3, i, j);
+						// max product algorithm
+						applyMaxProduct(_prob_max, otherValues, i, j);
 					}
 				}
 			}
 		}
 
-		//BER of sum-product
+		// sum-product error count
 		for (int i = 0; i < _num; i++)
 		{
 			for (int j = 0; j < 7; j++)
 			{
 				if (_prob[i][j][0] <= _prob[i][j][1])
 				{
-					_errorCount++;
+					_sum_errorCount++;
+				}
+				if (_prob_max[i][j][0] <= _prob_max[i][j][1])
+				{
+					_max_errorCount++;
 				}
 			}
 		}
 
 		// calculate the bit error rate
-		_BER = ((_errorCount / (7.0 * _num)) * 100);
+		_BER = ((_sum_errorCount / (7.0 * _num)) * 100);
 		std::cout << "The sum-product bit error rate is: (" << _BER << "%)" << std::endl;
+		_BER = ((_max_errorCount / (7.0 * _num)) * 100);
+		std::cout << "The max-product bit error rate is: (" << _BER << "%)" << std::endl;
 
 		// reset the counter
-		_errorCount = 0;
+		_sum_errorCount = 0;
+		_max_errorCount = 0;
 		_BER = 0.0;
 		_prob.clear();
+		_prob_max.clear();
 		std::cout << "---------------------------------------------------" << std::endl;
 		std::cout << "Do you want to do again? (0=yes; 1=no): ";
 		std::cin >> _again;
-		std::cout << "-----------------------------------------" << std::endl;
+		std::cout << "---------------------------------------------------" << std::endl;
 		if (_again == 0)
 		{
 			_start = 1;
@@ -194,7 +246,7 @@ void main()
 
 }
 
-void applyFunctionValue(std::vector<std::vector<std::vector<double>>> &prob,
+void applySumProduct(std::vector<std::vector<std::vector<double>>> &prob,
 	int originOtherValues[3], int i, int j){
 
 	int otherValues[3] = { originOtherValues[0] - 1, originOtherValues[1] - 1, originOtherValues[2] - 1 };
@@ -214,4 +266,36 @@ void applyFunctionValue(std::vector<std::vector<std::vector<double>>> &prob,
 
 	prob[i][j][0] = Util::instance()->logsum(prob[i][j][0], x0);
 	prob[i][j][1] = Util::instance()->logsum(prob[i][j][0], x1);
+}
+
+void applyMaxProduct(std::vector<std::vector<std::vector<double>>> &prob,
+	int originOtherValues[3], int i, int j){
+
+	int otherValues[3] = { originOtherValues[0] - 1, originOtherValues[1] - 1, originOtherValues[2] - 1 };
+
+	double x0, x1;
+	// calculate the probability when x is equal to 1
+	x1 = Util::instance()->returnBiggerValue(
+		Util::instance()->returnBiggerValue(
+		prob[i][otherValues[0]][1] * prob[i][otherValues[1]][1] * prob[i][otherValues[2]][1],
+		prob[i][otherValues[0]][1] * prob[i][otherValues[1]][0] * prob[i][otherValues[2]][0]),
+		Util::instance()->returnBiggerValue(
+		prob[i][otherValues[0]][0] * prob[i][otherValues[1]][1] * prob[i][otherValues[2]][0],
+		prob[i][otherValues[0]][0] * prob[i][otherValues[1]][0] * prob[i][otherValues[2]][1]));
+
+	// calculate the probability when x is equal to 0
+	x0 = Util::instance()->returnBiggerValue(
+		Util::instance()->returnBiggerValue(
+		prob[i][otherValues[0]][1] * prob[i][otherValues[1]][0] * prob[i][otherValues[2]][1],
+		prob[i][otherValues[0]][1] * prob[i][otherValues[1]][1] * prob[i][otherValues[2]][0]),
+		Util::instance()->returnBiggerValue(
+		prob[i][otherValues[0]][0] * prob[i][otherValues[1]][1] * prob[i][otherValues[2]][1],
+		prob[i][otherValues[0]][0] * prob[i][otherValues[1]][0] * prob[i][otherValues[2]][0]));
+
+	// normalize the x1 and x0
+	x1 = x1 / (x0 + x1);
+	x0 = x0 / (x0 + x1);
+
+	prob[i][j][0] = Util::instance()->returnBiggerValue(prob[i][j][0], x0);
+	prob[i][j][1] = Util::instance()->returnBiggerValue(prob[i][j][1], x1);
 }
